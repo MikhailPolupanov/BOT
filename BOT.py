124a9fc523921f8b532c6aa625183fedd67e52ac
Mikhail Polupanov <mihanren@mail.ru>
2022-06-21 23:16:25 +0300

    поправил по PEP8

--- BOT.py
+++ BOT.py
@@ -13,11 +13,20 @@
 
 """
 import logging
-from anketa import anketa_start, anketa_name, anketa_rating, anketa_skip, anketa_comment, anketa_dontknow
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
+from anketa import (
+    anketa_start, anketa_name, anketa_rating, anketa_skip, anketa_comment,
+    anketa_dontknow
+)
+from telegram.ext import (
+    Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
+)
 import settings
-from handlers import (greet_user, talk_to_me, guess_number, send_stalker_pict, 
-                        user_coordinates, word_count, full_moon, eph, check_user_photo, photo_request)
+from handlers import (
+    greet_user, talk_to_me, guess_number, send_stalker_pict,
+    user_coordinates, word_count, full_moon, eph, check_user_photo,
+    photo_request
+)
+
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -30,15 +39,20 @@ def main():
     dp = mybot.dispatcher
 
     anketa = ConversationHandler(
-		entry_points = [MessageHandler(Filters.regex('^(Заполнить анкету)$'), anketa_start)],
-		states = {
+      entry_points=[MessageHandler(Filters.regex('^(Заполнить анкету)$'),
+                    anketa_start)],
+      states={
         'name': [MessageHandler(Filters.text, anketa_name)],
-        'rating': [MessageHandler(Filters.regex('^(1|2|3|4|5)$'), anketa_rating)],
-        'comment': [CommandHandler('skip', anketa_skip), MessageHandler(Filters.text, anketa_comment)]
-		},
-		fallbacks = [
-			MessageHandler(Filters.text | Filters.photo | Filters.video | Filters.document | Filters.location, anketa_dontknow)
-		]
+        'rating': [MessageHandler(Filters.regex('^(1|2|3|4|5)$'),
+                                  anketa_rating)],
+        'comment': [CommandHandler('skip', anketa_skip),
+                    MessageHandler(Filters.text, anketa_comment)]
+      },
+      fallbacks=[MessageHandler(
+        Filters.text | Filters.photo | Filters.video | Filters.document |
+        Filters.location, anketa_dontknow
+      )
+      ]
     )
     dp.add_handler(anketa)
     dp.add_handler(CommandHandler("start", greet_user))
@@ -47,9 +61,11 @@ def main():
     dp.add_handler(CommandHandler("wordcount", word_count))
     dp.add_handler(CommandHandler("stalker", send_stalker_pict))
     dp.add_handler(CommandHandler("next_full_moon", full_moon))
-    dp.add_handler(MessageHandler(Filters.regex('^(Отправить картинку)$'), photo_request))
+    dp.add_handler(MessageHandler(Filters.regex('^(Отправить картинку)$'),
+                                  photo_request))
     dp.add_handler(MessageHandler(Filters.photo, check_user_photo))
-    dp.add_handler(MessageHandler(Filters.regex('^(Кто здесь)$'), send_stalker_pict))
+    dp.add_handler(MessageHandler(Filters.regex('^(Кто здесь)$'),
+                                  send_stalker_pict))
     dp.add_handler(MessageHandler(Filters.location, user_coordinates))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
